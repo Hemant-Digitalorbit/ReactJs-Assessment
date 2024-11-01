fab64319644016b1996a230449e7c88a4fa51889
Hemant-Digitalorbit <hemant.koli@digitalorbit.online>
2024-11-01 14:04:15 +0530

    form handling

---
+++ UsersForm Handling/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Components/Header'
+import DemoForm from './Components/DemoForm'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <DemoForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ UsersForm Handling/src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+        <Header>
+            <h1>Hello</h1>
+        </Header>
+    </div>
+  )
+}
